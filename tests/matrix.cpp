d7514fa783fb655b147c873b2707535264563272
5Dima0 <32190939+5Dima0@users.noreply.github.com>
2018-05-24 00:53:14 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -1,36 +1,48 @@
+/*Данный файл служит тестирующей проверкой правильности посторения программы. То есть, здесь проверяются определенные функции класса,
+проверяется правильность их выполнения, граничные случаи и поведение программы в случае ошибки*/
+
 #include <cassert>
 #include <catch.hpp>
 #include <sstream>
 
-#include "matrix.hpp"
+#include "matrix.hpp" //подключение файла с реализацией программы
 
-TEST_CASE("creating matrix")
+/*Используются так называемые макросы. В скобочках TEST_CASE пишется название теста, которое должно быть уникальным для каждой проверки.
+Далее идет стандартная программа на Си++, с объявлением переменных и определенными методами. В конце проверяются выходные данные с ожидаемыми
+посредством макроса REQUIRE, где проверяется логическое выражение. В случае REQUIRE(true) программа работает правильно и ожидаемое значение
+совпадает с реальным; в случае REQUIRE(false) поограмма работает неверно и выводится сообщение об ошибке*/
+
+TEST_CASE("creating matrix") //создание теста
 {
-    matrix_t<int> matrix;
-    REQUIRE( matrix.rows() == 0 );
-    REQUIRE( matrix.collumns() == 0 );
+    matrix_t<int> matrix; //создание матрицы с элементами типа целых чисел
+    REQUIRE( matrix.rows() == 0 ); //так как должен использоваться коструктор без аргументов, то значение строк и столбцов должно быть 0
+    REQUIRE( matrix.collumns() == 0 ); //через возвращаемые значения количества строк и столбцов и сравнения их с 0 получаем либо true, либо false
+    //так как после выполнения всех операторов блок программы завершается и, соответственно, завершается область видимости matrix, то в данном
+    //месте для нее вызывается деструктор
 }
 
 TEST_CASE("reading matrix")
 {
-    std::string input{
+    std::string input{ //создание строки, содержащей значение количесва строк, столбцов и самих ячеек матрицы
         "3, 3\n"
         "1 1 1\n"
         "2 2 2\n"
         "3 3 3" };
-    matrix_t<int> matrix;
+    matrix_t<int> matrix; //создание матрицы с элементами типа целых чисел
     std::istringstream stream{ input };
     
-    REQUIRE( matrix.read( stream ) );
-    REQUIRE( matrix.rows() == 3 );
+    REQUIRE( matrix.read( stream ) ); //инициализация матрицы через значения, введеные на консоль. Возвращаемое значение преобразуется в true либо false
+    REQUIRE( matrix.rows() == 3 ); //сравнение количества строк и столбцов с ожидаемыми значениями
     REQUIRE( matrix.collumns() == 3 );
     
     std::ostringstream output;
-    matrix.write( output );
+    matrix.write( output ); //инициализация потока значением вывода матрицы на экран
     
-    REQUIRE( input == output.str() );
+    REQUIRE( input == output.str() ); //сравнение вывода матрицы с ожидаемым выводом программы
+    //здесь вызывается деструктор
 }
 
+//Аналогичный пример, однако теперь создается экземпляр матрицы с вещественными числами
 TEST_CASE("reading float_matrix")
 {
     std::string input{
@@ -51,8 +63,10 @@ TEST_CASE("reading float_matrix")
     REQUIRE( input == output.str() );
 }
 
+//Проверка правильности сложения матриц
 TEST_CASE("addings matrixs")
 {
+    //Создаются и инициализируются строками с консоли две матрицы вещественного типа
     std::string first_matrix_representation{
         "1, 3\n"
         "1.1 1.1 1.1\n"
@@ -70,7 +84,7 @@ TEST_CASE("addings matrixs")
     matrix_t<float> second_matrix;
     second_matrix.read( stream_ );
     
-    matrix_t<float> result_matrix = first_matrix + second_matrix;
+    matrix_t<float> result_matrix = first_matrix + second_matrix; //создается матрица, здесь так же используется оператор копирующего присваивания
 
     std::string expected_result_matrix_representation{
         "1, 3\n"
@@ -80,9 +94,10 @@ TEST_CASE("addings matrixs")
     std::ostringstream output;
     result_matrix.write (output);
     
-    REQUIRE( output.str() == expected_result_matrix_representation );
+    REQUIRE( output.str() == expected_result_matrix_representation ); //сравнение вывода программы с ожидаемой получившейся матрицей
 }
 
+//Аналогично для разности матриц
 TEST_CASE("subtracting matrixs")
 {
     std::string first_matrix_representation{
@@ -115,6 +130,7 @@ TEST_CASE("subtracting matrixs")
     REQUIRE( output.str() == expected_result_matrix_representation );
 }
 
+//Аналогично для произведения
 TEST_CASE("multiplying matrixs")
 {
     std::string first_matrix_representation{
@@ -149,46 +165,10 @@ TEST_CASE("multiplying matrixs")
     REQUIRE( output.str() == expected_result_matrix_representation );
 }
 
-TEST_CASE("addings matrixs 3x1")
+//Проверка генерации исключения
+TEST_CASE("experiment")
 {
-    std::string first_matrix_representation{
-        "3, 1\n"
-        "1\n"
-        "1\n"
-        "1\n"
-    };
-    std::string second_matrix_representation{
-        "3, 1\n"
-        "1\n"
-        "1\n"
-        "1\n"
-    };
-    
-    std::istringstream stream{ first_matrix_representation };
-    matrix_t<int> first_matrix;
-    first_matrix.read( stream );
-    
-    std::istringstream stream_ { second_matrix_representation };
-    matrix_t<int> second_matrix;
-    second_matrix.read( stream_ );
-    
-    matrix_t<int> result_matrix = first_matrix + second_matrix;
-
-    std::string expected_result_matrix_representation{
-        "3, 1\n"
-        "2\n"
-        "2\n"
-        "2\n"
-    };
-    
-    std::ostringstream output;
-    result_matrix.write (output);
-    
-    REQUIRE( output.str() == expected_result_matrix_representation );
-}
-
-TEST_CASE("experiment_1")
-{
+    //Создаются две матрицы, которые не могут умножаться по определению
     std::string first_matrix_representation{
         "1, 1\n"
         "1.1\n"
@@ -207,28 +187,8 @@ TEST_CASE("experiment_1")
     matrix_t<float> second_matrix;
     second_matrix.read( stream_ );
     
+    //Макрос REQUIRE_THROWS_AS проверяет, какое именно исключение было сгенерировано во время выполнения функции
+    //Так как умножение матрицы сгенерирует исключение, то если сгенерированное исключение будет таким же, как ожидаемое, то макрос сгенерирует
+    //true и программа завершится правильно. В противном случае она завершится аварийно
     REQUIRE_THROWS_AS( (first_matrix * second_matrix ), std::invalid_argument);
-}
-
-TEST_CASE("experiment_2")
-{
-    std::string first_matrix_representation{
-        "1, 1\n"
-        "1.1\n"
-    };
-    std::string second_matrix_representation{
-        "2, 1\n"
-        "1.0\n"
-        "1.0\n"
-    };
-    
-    std::istringstream stream{ first_matrix_representation };
-    matrix_t<float> first_matrix;
-    first_matrix.read( stream );
-    
-    std::istringstream stream_ { second_matrix_representation };
-    matrix_t<float> second_matrix;
-    second_matrix.read( stream_ );
-    
-    REQUIRE_THROWS_AS( (first_matrix - second_matrix ), std::invalid_argument);
 }
